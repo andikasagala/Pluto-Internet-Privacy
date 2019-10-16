539068df2e728018aae845b4951e920d60e6fbaa
Andika Sagala <46408369+andikasagala@users.noreply.github.com>
2019-10-16 20:46:39 +0700

    Pluto Internet Privacy
    Pluto Internet Privacy

--- stuffs/PAFreeBSD.sh
+++ stuffs/PAFreeBSD.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 
-		
-	echo "---== Pluto Internet Privacy ==---" 
+
+
+
+
+	echo "---== Pluto Internet Privacy ==---"
 	echo "Server options:"
 	echo "1. Public Access"
 	echo "2. Private Access"
@@ -10,14 +13,14 @@
 	read MyaccessFreeBSD
 	case "$MyaccessFreeBSD" in
 	        1)
-			echo "a. Tor mode" 
-			echo "b. OpenVPN mode" 
-			echo "c. IPsec/L2TP mode" 
-			echo "d. SoftEther VPN mode" 
-			echo "Option mode a/b/c/d:" 
+			echo "a. Tor mode"
+			echo "b. OpenVPN mode"
+			echo "c. IPsec/L2TP mode"
+			echo "d. SoftEther VPN mode"
+			echo "Option mode a/b/c/d:"
 			read PublicAccessMode
 			case "$PublicAccessMode" in
-				a) 
+				a)
 				FreeBSDSquidRun=`killall squid;/usr/local/sbin/squid -k parse;/usr/local/sbin/squid -f /usr/local/etc/squid/squid.conf`
 				FreeBSDPrivoxyRun=`killall privoxy;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config2;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config3;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config4;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config5;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config6;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config7;/usr/local/sbin/privoxy /usr/local/etc/privoxy/config8`
 				FreeBSDTorRun=`killall tor;/usr/local/bin/tor -f /etc/tor/torrc;/usr/local/bin/tor -f /etc/tor/torrc2;/usr/local/bin/tor -f /etc/tor/torrc3;/usr/local/bin/tor -f /etc/tor/torrc4;/usr/local/bin/tor -f /etc/tor/torrc5;/usr/local/bin/tor -f /etc/tor/torrc6;/usr/local/bin/tor -f /etc/tor/torrc7;/usr/local/bin/tor -f /etc/tor/torrc8`
@@ -25,34 +28,33 @@
 				echo $FreeBSDTorRun
 				echo $FreeBSDPrivoxyRun
 				echo $FreeBSDSquidRun
-				printf "\n\n\n" 
+				printf "\n\n\n"
 				echo $VarIPPortLogQuery
 				printf "\n"
-				echo "Connected. Run: sh /usr/bin/EarthPlanet/Shutdown if stuck." 
-						
+				echo "Connected. Run: sh /usr/bin/EarthPlanet/Shutdown if stuck."
+
 				echo "`date` - Connected to Public Access. Tor mode."  >> /var/log/pagan.log
 		                ;;
-				b) 
+				b)
 				echo "OpenVPN in progress of development..."
 				;;
-				c) 
+				c)
 				echo "IPsec/L2TP in progress of development..."
 				;;
-				d) 
+				d)
 				echo "SoftEther in progress of development..."
 				;;
-				
 				*)
 				echo "None, bye.."
 				exit 0
 				;;
-		    esac	 
+			esac
 			;;
-			2) 
-			echo "Private Server Access Will available soon in next version update." 
+			2)
+			echo "Private Server Access Will available soon in next version update."
 			;;
-			3) 
-			echo "Science access is a access to server which you can do Networking Experiment, Operating System, Scientific Software, etc inside the server. Science Server Access Will available soon in next version update." 
+			3)
+			echo "Science access is a access to server which you can do Networking Experiment, Operating System, Scientific Software, etc inside the server. Science Server Access Will available soon in next version update."
 			;;
 
 			*)
@@ -64,5 +66,4 @@
 
 
 	echo "`date` - Creating file /usr/bin/EarthPlanet/GoToPluto done 2."  >> /var/log/pagan.log
-		
 
